99ab25dd2c2a17bc77a0db8f2472083d75e04ce3
Hosein Parvaresh <139824541+HoseinParvaresh@users.noreply.github.com>
2025-04-22 20:01:01 +0330

    trustUs section part 1 finish

--- src/components/templates/Home/TrustUs/TrustUs.js
+++ src/components/templates/Home/TrustUs/TrustUs.js
@@ -1,7 +1,38 @@
+import Link from "next/link";
+import { MdOutlinePets } from "react-icons/md";
 export default function TrustUs() {
     return (
-        <div className="px-27.5 bg-[url(/images/main-page-pic-4.jpg)] h-120">
-            Enter
+        <div className="grid grid-cols-2 pt-16 pb-25 px-27.5 bg-[url(/images/main-page-pic-4.jpg)] h-120">
+            {/* right */}
+            <div>
+                <p className="title text-white">دلایـلی کـه میـتوانیـد به مجموعه ما اعتـماد کنیـد</p>
+                <p className="title-desc text-white max-w-[450px] mt-5">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. لورم ایپسوم متن ساختگی با تولید سادگی است.</p>
+               {/* button */}
+                <Link href={'/'} className='btn btn-primary--light mt-5 animate__animated animate__fadeInUp'> 
+                    <span>درباره پیلیسوک</span>
+                    <MdOutlinePets className="size-4"/>
+                </Link>
+            </div>
+
+            {/* left */}
+            <div className="grid grid-cols-2 gap-5">
+                <div className="bg-white/15 flex-center flex-col size-full rounded-xl text-white">
+                    <span className="font-danaBold text-5xl">250<span className="text-3xl">+</span></span>
+                    <span className="font-Morabba text-2xl">مشتری راضی</span>
+                </div>
+                <div className="bg-white/15 flex-center flex-col size-full rounded-xl text-white">
+                    <span className="font-danaBold text-5xl">10<span className="text-3xl">+</span></span>
+                    <span className="font-Morabba text-2xl">سال سابقه</span>
+                </div>
+                <div className="bg-white/15 flex-center flex-col size-full rounded-xl text-white">
+                    <span className="font-danaBold text-5xl">40<span className="text-3xl">+</span></span>
+                    <span className="font-Morabba text-2xl">اعضای تیم</span>
+                </div>
+                <div className="bg-white/15 flex-center flex-col size-full rounded-xl text-white">
+                    <span className="font-danaBold text-5xl">250<span className="text-3xl">+</span></span>
+                    <span className="font-Morabba text-2xl">حیوان خوشحال</span>
+                </div>
+            </div>
         </div>
     );
 }
