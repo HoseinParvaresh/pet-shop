33f1e44ec920121fdcbf140e05d6580de911e65f
Hosein Parvaresh <139824541+HoseinParvaresh@users.noreply.github.com>
2025-05-01 05:35:57 +0330

    respansive joinNewesLetter section

--- src/components/templates/Home/JoinNewsletter/JoinNewsletter.js
+++ src/components/templates/Home/JoinNewsletter/JoinNewsletter.js
@@ -1,21 +1,25 @@
+import { MdOutlinePets } from "react-icons/md";
+
 export default function JoinNewsletter() {
     return (
         <div className="bg-secondary mt-30">
-            <div className="container pb-10 grid grid-cols-2">
-                <div data-aos="fade-down" className="flex flex-col pt-10 text-center gap-3">
+            <div className="container pb-5 grid grid-cols-1 md:grid-cols-2 items-center lg:items-start">
+                <div data-aos="fade-down" className="flex flex-col pt-15 md:pt-25 gap-3 text-center md:text-right items-center md:items-start">
                     <p className="head-title">عضویت خبرنامه </p>
-                    <p className="title max-w-110 text-right">در خبـرنامه ما عضـو شویـد</p>
-                    <p className="title-desc text-right">
+                    <p className="title max-w-110">در خبـرنامه ما عضـو شویـد</p>
+                    <p className="title-desc line-clamp-2">
                     با عضویت در خبرنامه ما میتوانید از جدیدترین مقالات و تخفیف های ما باخبر شوید.
                     </p>
-                    <div className=" bg-white outline outline-black/10 w-max rounded-lg mt-3">
-                        <input placeholder="ایمیل شما" type="text" className=" outline-none pl-40 pr-5 py-3" />
+                    <div className="bg-white outline outline-black/10 w-full md:w-100 rounded-lg mt-3">
+                        <input placeholder="ایمیل شما" type="text" className=" outline-none w-full py-3 px-3 md:px-5" />
+                    </div>
+                    <div className="btn md:btn-xl btn-primary mt-5">
+                        <p>عضویت</p>
+                        <MdOutlinePets className="size-5" />            
                     </div>
                 </div>
-                <div>
-                    {/* image */}
-                    <img src="/images/main-page-pic-5.png" alt="main-page-pic-5" />
-                </div>
+                {/* image */}
+                <img data-aos="fade-up" src="/images/main-page-pic-5.png" alt="main-page-pic-5" />
             </div>
         </div>
     );
