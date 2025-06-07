9779022b2af35e57fdb5e9b3e69765976bf83239
Hosein Parvaresh <139824541+HoseinParvaresh@users.noreply.github.com>
2025-06-07 05:25:42 +0330

    fix bug

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,9 +14,9 @@ export default function App({ Component, pageProps }) {
 
   useEffect(() => {
     AOS.init();
-    window.addEventListener('scroll',() => {
-      customScroll("#cat-scroll")
-    })
+    // window.addEventListener('scroll',() => {
+    //   customScroll("#cat-scroll")
+    // })
   }, [])
 
   return (
@@ -24,7 +24,7 @@ export default function App({ Component, pageProps }) {
         <ParallaxProvider>
           <Svg/>
           <Header/>
-            <Image width={160} height={10} id="cat-scroll" className="fixed z-50 -right-50 top-0 hidden xl:block" src="/images/cat-2.png" alt="cat-scroll"/>
+            {/* <Image width={160} height={10} id="cat-scroll" className="fixed z-50 -right-50 top-0 hidden xl:block" src="/images/cat-2.png" alt="cat-scroll"/> */}
             <Component {...pageProps} />
           <Footer/>
         </ParallaxProvider>
