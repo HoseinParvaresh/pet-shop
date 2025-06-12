0a127f7dc8bd87c00b4af2897ef691ce79be76ed
Hosein Parvaresh <139824541+HoseinParvaresh@users.noreply.github.com>
2025-06-12 22:25:04 +0330

    error handler for signup

--- src/Utility/Alert.js
+++ src/Utility/Alert.js
@@ -3,23 +3,21 @@ import toast from "react-hot-toast"
 export default function Alert(order,text) {
   return toast.custom(() => (
     <div
-    className={`bg-white border-b-3 ${order === 'error' ? 'border-red-500' : 'border-primary'} shadow-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5 overflow-hidden`}
-  >
+    className={`bg-white border-b-3 ${order === 'error' ? 'border-red-500' : 'border-green-500'} shadow-lg flex overflow-hidden`}>
       {/* icon & text */}
-      <div className="flex items-start p-4">
+      <div className="flex items-start px-4 py-3">
       {/* icon */}
         <div className="flex-shrink-0 pt-0.5 pl-3">
-          <svg className={`size-11 ${order === 'error' ? 'text-red-500' : 'text-primary'}`}>
+          <svg className={`size-11 ${order === 'error' ? 'text-red-500' : 'text-green-500'}`}>
             <use href={`${order === 'error' ? '#x-circle' : '#check-circle' }`}></use>
           </svg>
         </div>
         {/* text */}
         <div className="ml-3 flex-1">
-          <p className="text-base font-DanaBold text-gray-900">
+          <p className="text-base font-danaBold text-gray-900">
             {order === 'error' ? 'خطا' : 'موفق'}
           </p>
-          <p className="mt-1 text-sm text-gray-500
-          ">
+          <p className="mt-1 text-sm text-gray-600">
             {text}
           </p>
         </div>
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -2,17 +2,20 @@ import React, { useState } from 'react';
 import apiRequests from '@/Services/Axios/Configs/Configs';
 import { v4 as uuidv4 } from 'uuid';
 import { isValidEmail } from '@/Utility/UtilityFunction';
-import { token } from '@/Utility/Constants';
+import Alert from '@/Utility/Alert';
 
 export default function Signup() {
 
     const [email, setEmail] = useState("")
-    const username = uuidv4()
-    const password = Math.random().toString(36).slice(2, 12)
+    const [stepRegister,setStepRegister] = useState("hidden")
 
     const signup = async () => {
 
+        const username = uuidv4()
+        const password = Math.random().toString(36).slice(2, 12)
+
         if(!isValidEmail(email)) {
+            Alert("error","آدرس ایمیل وارد شده اشتباه است")
             return false
         }
 
@@ -23,9 +26,19 @@ export default function Signup() {
             confirm_password: password
         }
 
-        apiRequests.post('/users/register/',data).then(res => {
-            console.log(res);
-        }).catch(error => console.log(error)); 
+        apiRequests.post('/users/register/',data)
+        .then(res => {
+            if(res.status === 201) {
+                return Alert("successful","کد تأیید به ایمیل شما ارسال شد")
+            }
+        }).catch(error => {
+            if(error.status === 400) {
+                return Alert("error","آدرس ایمیل وارد شده قبلا استفاده شده است")
+            }
+            if(error.status === 500) {
+                return Alert("error","از سمت سرور مشکلی پیش اومده")
+            }
+        }); 
 
     }
 
@@ -42,6 +55,17 @@ export default function Signup() {
                     <div className="max-w-md mx-auto">
                         <div>
                             <h1 className="text-2xl mb-1 font-MorabbaBold">عضویت در پیلیسوک</h1>
+                        </div>
+                        <div className="divide-y divide-gray-200">
+                            <div className="py-8  leading-6 space-y-4 text-gray-700 sm:leading-7">
+                                <div className="relative">
+                                    <input value={email} onChange={e => setEmail(e.target.value)} autoComplete="off" autoFocus id="email" name="email" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="آدرس ایمیل" />
+                                    <label htmlFor="email" className="absolute right-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-md peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-xs">آدرس ایمیل</label>
+                                </div>
+                                <div className="relative">
+                                    <button onClick={signup} className="btn btn-primary py-1.5 w-full">ادامه</button>
+                                </div>
+                            </div>
                         </div>
                         <div className="divide-y divide-gray-200">
                             <div className="py-8  leading-6 space-y-4 text-gray-700 sm:leading-7">
