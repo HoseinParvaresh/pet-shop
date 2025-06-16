e89efe7dfa6567d295292623b00f13a22eb163f5
Hosein Parvaresh <139824541+HoseinParvaresh@users.noreply.github.com>
2025-06-16 22:27:52 +0330

    auth context

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,18 +5,39 @@ import 'animate.css';
 import { ParallaxProvider } from 'react-scroll-parallax';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Footer from "@/components/modules/Footer/Footer";
 import AuthContext from "@/context/authContext";
 
 export default function App({ Component, pageProps }) {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [token, setToken] = useState(null)
+  const [userInfo, setUserInfo] = useState(null)
+
+  const login = (token) => {
+    setToken(token)
+    localStorage.setItem('user', JSON.stringify({ token }))
+  }
+  const logout = () => {
+    setToken(null)
+    setUserInfo({})
+    localStorage.removeItem('user')
+  }
+
 
   useEffect(() => {
     AOS.init();
   }, [])
 
   return (
-    <AuthContext.Provider>
+    <AuthContext.Provider value={{
+      isLoggedIn,
+      token,
+      userInfo,
+      login,
+      logout
+    }}>
       <div className="font-dana">
         <ParallaxProvider>
           <Svg />
