c8b287c6e16bfbe6aa0ac580ea924c70bd8b2c62
Hosein Parvaresh <139824541+HoseinParvaresh@users.noreply.github.com>
2025-06-20 06:59:50 +0330

    JoinNewsletter send email active

--- src/components/templates/Home/IntroducingProducts/IntroducingProducts.js
+++ src/components/templates/Home/IntroducingProducts/IntroducingProducts.js
@@ -6,7 +6,7 @@ export default function IntroducingProducts() {
         <div className="container my-15 text-dark-primary">
             <div className="flex flex-col lg:flex-row justify-between shadow-primary rounded-xl md:rounded-2xl relative">
                 <div className="p-8 md:p-10 flex flex-col gap-5 md:gap-7">
-                    <p className="font-MorabbaHeavy text-2xl md:text-4xl">بـهـتـرین مـحـصـولات در <p className="text-primary mr-2 mt-2 inline-block">پـیـلـسـوک</p></p>
+                    <p className="font-MorabbaHeavy text-2xl md:text-4xl">بـهـتـرین مـحـصـولات در <span className="text-primary mr-2 mt-2 inline-block">پـیـلـسـوک</span></p>
                     <p className="line-clamp-2 text-sm md:text-base">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای تکنولوژی است.</p>
                     <Link href={"/store"} className="btn btn-primary px-4 py-2.5 text-sm md:px-6 md:py-2 md:text-base">
                         <span>مشاهده محصولات</span>
--- src/components/templates/Home/JoinNewsletter/JoinNewsletter.js
+++ src/components/templates/Home/JoinNewsletter/JoinNewsletter.js
@@ -1,7 +1,20 @@
 import { MdOutlinePets } from "react-icons/md";
 import Alert from "@/Utility/Alert";
+import { isValidEmail } from "@/Utility/UtilityFunction";
+import { useCallback, useState } from "react";
 
 export default function JoinNewsletter() {
+
+    const [email,setEmail] = useState("")
+
+    const joinNewsletter = () => {
+        if(!isValidEmail(email)) {
+            Alert("error","ایمیل وارد شده نامعتبر است")
+            return 
+        }
+        Alert("success","ایمیل شما با موفقیت ثبت شد")
+
+    }
     return (
         <div className="bg-secondary">
             <div className="container pb-5 grid grid-cols-1 md:grid-cols-2 items-center lg:items-start">
@@ -15,10 +28,10 @@ export default function JoinNewsletter() {
                     </p>
                     {/* input (search) */}
                     <div className="bg-white outline outline-black/10 w-full md:w-100 rounded-lg mt-3">
-                        <input placeholder="ایمیل شما" type="text" className=" outline-none w-full py-3 px-3 md:px-5" />
+                        <input value={email} onChange={(e)=> setEmail(e.target.value)} placeholder="ایمیل شما" type="text" className=" outline-none w-full py-3 px-3 md:px-5" />
                     </div>
                     {/* button */}
-                    <div className="btn md:px-6.5 md:py-3.5  btn-primary mt-5">
+                    <div onClick={joinNewsletter} className="btn btn-primary btn-xl mt-5">
                         <p>عضویت</p>
                         <MdOutlinePets className="size-5" />
                     </div>
