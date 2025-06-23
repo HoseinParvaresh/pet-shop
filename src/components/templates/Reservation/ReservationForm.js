e01f6b562b993dae7e7698e78a85de64ed7f3740
Hosein Parvaresh <139824541+HoseinParvaresh@users.noreply.github.com>
2025-06-23 10:04:54 +0330

    reservation section part 1 complete

---
+++ src/components/templates/Reservation/ReservationForm.js
@@ -0,0 +1,77 @@
+export default function ReservationForm() {
+    return (
+        <div className="container grid grid-cols-2 mb-30 gap-10">
+            <form className='px-3 space-y-7 md:space-y-8 bg-gray-100 p-5 rounded-lg'>
+                <p className="font-MorabbaBold text-2xl">درخواست رزرو</p>
+                {/* first name and last name / number */}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-5">
+                    {/* first name and last name */}
+                    <div>
+                        <label className="inline-block font-danaBold text-sm mb-2 md:mb-3">نام و نام خانوادگی</label>
+                        <input type="text" placeholder='' className="w-full h-13 placeholder:text-slate-500 text-gray-900 bg-white text-sm py-3.5 pr-3.5 pl-13 rounded outline-none" required="" />
+                    </div>
+                    {/* number */}
+                    <div>
+                        <label className="inline-block font-danaBold text-sm mb-2 md:mb-3"> شماره تلفن </label>
+                        <input type="text" placeholder='' className="w-full h-13 placeholder:text-slate-500 text-gray-900 bg-white text-sm py-3.5 pr-3.5 pl-13 rounded outline-none" required="" />
+                    </div>
+                </div>
+                {/* type of animal /  breed of animal*/}
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-5'>
+                    {/* type of animal */}
+                    <div>
+                        <label className="inline-block font-danaBold text-sm mb-2 md:mb-3"> حیوان  </label>
+                        <select name="category" required="" className="w-full h-13 text-slate-500 bg-white text-sm p-3.5 border-l-[14px] border-l-transparent rounded outline-none">
+                            <option value={false}> حیوانت چیه؟ </option>
+                            <option value="سگ"> سگ </option>
+                            <option value="گربه"> گربه </option>
+                            <option value="پرنده"> پرنده </option>
+                            <option value="همستر">همستر</option>
+                        </select>
+                    </div>
+                    {/* breed of animal */}
+                    <div>
+                        <label className="inline-block font-danaBold text-sm mb-2 md:mb-3"> نژاد </label>
+                        <input type="text" placeholder='نژادش چیه؟' className="w-full h-13 placeholder:text-slate-500 text-gray-900 bg-white text-sm py-3.5 pr-3.5 pl-13 rounded outline-none" required="" />
+                    </div>
+                </div>
+                {/* reason for referral / date */}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-5">
+                    {/* reason for referral */}
+                    <div>
+                        <label className="inline-block font-danaBold text-sm mb-2 md:mb-3"> علت مراجعه  </label>
+                        <select name="category" required="" className="w-full h-13 text-slate-500 bg-white text-sm p-3.5 border-l-[14px] border-l-transparent rounded outline-none">
+                            <option value={false}> چه کاری میخوای برات انجام بدیم؟ </option>
+                            <option value="واکسن"> واکسن </option>
+                            <option value="اصلاح"> اصلاح </option>
+                            <option value="پانسیون"> پانسیون </option>
+                            <option value="زندگی‌نامه">همستر</option>
+                            <option value="روان‌شناسی">ماهی</option>
+                        </select>
+                    </div>
+                    {/* date */}
+                    <div>
+                        <label className="inline-block font-danaBold text-sm mb-2 md:mb-3"> تاریخ </label>
+                        <select name="category" required="" className="w-full h-13 text-slate-500 bg-white text-sm p-3.5 border-l-[14px] border-l-transparent rounded outline-none">
+                            <option value={false}> چه کاری میخوای برات انجام بدیم؟ </option>
+                            <option value="واکسن"> واکسن </option>
+                            <option value="اصلاح"> اصلاح </option>
+                            <option value="پانسیون"> پانسیون </option>
+                            <option value="زندگی‌نامه">همستر</option>
+                            <option value="روان‌شناسی">ماهی</option>
+                        </select>
+                    </div>
+
+                </div>
+                {/* Additional information */}
+                <label className="inline-block font-danaBold text-sm mb-2 md:mb-3"> توضیحات تکمیلی </label>
+                <textarea className="w-full min-h-30 placeholder:text-slate-500 text-gray-900 bg-white text-sm p-3.5 outline-none rounded" placeholder="سابقه درمانی ، موارد که بهشون حساسیت داره ، سابقه واکسن  و..." id="text" name="text" required=""></textarea>
+                {/* button */}
+                <button className="btn btn-primary w-full">  ارسال </button>
+            </form>
+            <div className="rounded-lg overflow-hidden">
+                <img className="h-full" src="/images/cat4.jpg" alt="cat4" />
+            </div>
+        </div>
+    );
+}
--- src/pages/reservation/index.js
+++ src/pages/reservation/index.js
@@ -1,62 +1,10 @@
-import CatPawTrail from "@/components/templates/Reservation/CatPawTrail";
-
+import ReservationHead from "@/components/templates/Reservation/ReservationHead";
+import ReservationForm from "@/components/templates/Reservation/ReservationForm";
 export default function Reservation() {
     return (
-        <div>
-            <div className="h-30 bg-primary flex items-center justify-around w-full relative mb-30 overflow-hidden bg__footer">
-                {/* right */}
-                <div className="z-10">
-                    <p className="font-MorabbaBold text-4xl text-dark-primary">رزرو نوبت</p>
-                </div>
-                {/* left */}
-                <div>
-                    {/* <img className="w-70" src="/images/cat-2.png" alt="cat" /> */}
-                </div>
-            </div>
-            <div className="container grid grid-cols-2 mb-30 gap-10">
-                <form className='px-3 space-y-7 md:space-y-8 bg-gray-100 p-5 rounded-lg'>
-                    <p className="font-MorabbaBold text-2xl">درخواست رزرو</p>
-                    {/* book name */}
-                    <div className="relative">
-                        <label className="inline-block font-Dana text-sm mb-2 md:mb-3">عنوان کتاب</label>
-                        <input type="text" placeholder='عنوان کتاب' className="w-full h-13 placeholder:text-slate-500 text-gray-900 bg-white text-sm py-3.5 pr-3.5 pl-13 rounded outline-none" required="" />
-                    </div>
-                    {/* book status / book category / book province */}
-                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-5'>
-                        {/* book category */}
-                        <div>
-                            <label className="inline-block font-Dana text-sm mb-2 md:mb-3"> دسته بندی </label>
-                            <select name="category" required="" className="w-full h-13 text-slate-500 bg-white text-sm p-3.5 border-l-[14px] border-l-transparent rounded outline-none">
-                                <option value={false}> موضوعش چیه؟ </option>
-                                <option value="داستان و رمان">داستان و رمان</option>
-                                <option value="مدیریت و کسب و کار">مدیریت و کسب و کار</option>
-                                <option value="ادبیات">ادبیات</option>
-                                <option value="روان‌شناسی">روان‌شناسی</option>
-                                <option value="زندگی‌نامه">زندگی‌نامه</option>
-
-                            </select>
-                        </div>
-                        {/* book status */}
-                        <div>
-                            <label className="inline-block text-sm mb-2 md:mb-3"> وضعیت </label>
-                            <select name="status" required="" className="w-full h-13 text-slate-500 bg-white text-sm p-3.5 border-l-[14px] border-l-transparent rounded outline-none">
-                                <option value={false}> کتابت در چه حاله؟ </option>
-                                <option value="کاملا نو">کاملا نو</option>
-                                <option value="در حد نو">در حد نو</option>
-                                <option value="آسیب دیده"> آسیب دیده </option>
-                            </select>
-                        </div>
-                    </div>
-                    {/* description */}
-                    <textarea className="w-full min-h-48 placeholder:text-slate-500 text-gray-900 bg-white text-sm p-3.5 outline-none rounded" placeholder="توضیحات" id="text" name="text" required=""></textarea>
-                    {/* button */}
-                    <button className={`btn btn-primary sm:w-62 `}> ثبت اطلاعات کتاب </button>
-                </form>
-                <div className="rounded-lg overflow-hidden">
-                    <img src="/images/cat4.jpg" alt="cat4" />
-                </div>
-            </div>
-
-        </div>
+        <>
+            <ReservationHead/>
+            <ReservationForm/>
+        </>
     );
 }
