0ab3078ceab9113f26c586fbd40b81cfa85b7681
Hosein Parvaresh <139824541+HoseinParvaresh@users.noreply.github.com>
2025-07-13 08:13:40 +0330

    updata StatisticsCardsItem component

--- src/components/templates/AdminDashboard/StatisticsCards/StatisticsCards.js
+++ src/components/templates/AdminDashboard/StatisticsCards/StatisticsCards.js
@@ -4,35 +4,17 @@ import { HiUsers } from "react-icons/hi2";
 import { HiOutlineEye } from "react-icons/hi";
 export default function StatisticsCards() {
 
-    const chartDataSalary = [
-        { "درآمد": 1258755 },
-        { "درآمد": 1056000 },
-        { "درآمد": 3258596 },
-        { "درآمد": 1002586 },
-        { "درآمد": 1354120 },
-        { "درآمد": 2685000 },
+    const chartData = [
+        { month: "4 هفته قبل", data: 2 },
+        { month: "3 هفته قبل", data: 50 },
+        { month: "2 هفته قبل", data: 30 },
+        { month: "هفته قبل", data: 25 },
     ]
-    const chartDataCustomers = [
-        { "مشتریان": 165 },
-        { "مشتریان": 150 },
-        { "مشتریان": 115 },
-        { "مشتریان": 132 },
-        { "مشتریان": 100 },
-        { "مشتریان": 130 },
-    ]
-    const chartDataVisit = [
-        { "بازدید از فروشگاه": 15000 },
-        { "بازدید از فروشگاه": 25000 },
-        { "بازدید از فروشگاه": 20000 },
-        { "بازدید از فروشگاه": 30000 },
-        { "بازدید از فروشگاه": 45000 },
-        { "بازدید از فروشگاه": 32000 },
-    ]
-    
+
     const StatisticsCardsData = [
-        { id: 1, title: "درآمد", number: 268500, percent: 20, benefit: true, chartData: chartDataSalary, color: "#82ca9d", icon: <FaWallet /> },
-        { id: 2, title: "مشتریان", number: 130, percent: 15, benefit: true, chartData: chartDataCustomers, color: "#8884d8", icon: <HiUsers /> },
-        { id: 3, title: "بازدید از فروشگاه", number: 32000, percent: 5, benefit: false, chartData: chartDataVisit, color: "#ffc658", icon: <HiOutlineEye /> },
+        { id: 1, title: "درآمد", chartData, color: "#82ca9d", icon: <FaWallet /> },
+        { id: 2, title: "مشتریان", chartData, color: "#8884d8", icon: <HiUsers /> },
+        { id: 3, title: "بازدید از فروشگاه", chartData, color: "#ffc658", icon: <HiOutlineEye /> },
     ]
 
     return (
--- src/components/templates/AdminDashboard/StatisticsCards/StatisticsCardsItem.js
+++ src/components/templates/AdminDashboard/StatisticsCards/StatisticsCardsItem.js
@@ -1,41 +1,31 @@
 "use client"
 
-import { TrendingUp } from "lucide-react"
-import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
+import { Area, AreaChart, XAxis } from "recharts"
 import { formatNumber } from "@/Utility/UtilityFunction"
 import { TiArrowSortedUp } from "react-icons/ti"
 import { TiArrowSortedDown } from "react-icons/ti"
 
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/shadcn/card"
-import {
-    ChartContainer,
-    ChartTooltip,
-    ChartTooltipContent,
-} from "@/components/shadcn/chart"
-
-
-const chartData = [
-    { month: "4 هفته قبل", desktop: 2 },
-    { month: "3 هفته قبل", desktop: 50 },
-    { month: "2 هفته قبل", desktop: 30 },
-    { month: "هفته قبل", desktop: 25 },
-]
-
-const chartConfig = {
-    desktop: {
-        label: "Desktop",
-        color: "var(--chart-1)",
-    }
-}
+import { Card, CardContent, CardHeader, } from "@/components/shadcn/card"
+import { ChartContainer, ChartTooltip, ChartTooltipContent, } from "@/components/shadcn/chart"
 
 export default function StatisticsCardsItem(Props) {
+
+    const lastData = Props.chartData[Props.chartData.length - 1].data
+    const secondLastData = Props.chartData[Props.chartData.length - 2].data
+
+    const chartConfig = {
+        data: {
+            label: "Desktop",
+            color: `${Props.color}`,
+        }
+    }
+
+    function calcProfitLossPct(firstData, secondData) {
+        if (firstData < secondData) {
+            return Math.floor(((secondData - firstData) / firstData) * 100) + "%"
+        }
+        return Math.floor(((firstData - secondData) / secondData) * 100) + "%"
+    }
     return (
         <Card className={"gap-0 pt-4 pb-1.5 shadow-none border-none"}>
             <CardHeader>
@@ -43,19 +33,19 @@ export default function StatisticsCardsItem(Props) {
                     {/* left => title / price / percent */}
                     <div className="flex flex-col gap-0.5">
                         <p className="text-sm font-danaBold">{Props.title}</p>
-                        <p className="text-sm">{formatNumber(Props.number)}</p>
-                        <div className={`flex-center ${Props.benefit ? 'text-green-500' : 'text-red-500'} text-xs`}>
+                        <p className="text-sm">{formatNumber(lastData)}</p>
+                        <div className={`flex-center ${secondLastData < lastData ? 'text-green-500' : 'text-red-500'} text-xs`}>
                             <div className="*:size-5">
-                                {Props.benefit ? <TiArrowSortedUp /> : <TiArrowSortedDown />}
+                                {secondLastData < lastData ? <TiArrowSortedUp /> : <TiArrowSortedDown />}
                             </div>
                             <div className="flex-center gap-1">
-                                <span>{Props.percent}%</span>
+                                <span>{calcProfitLossPct(lastData, secondLastData)}</span>
                                 <span>هفته قبل</span>
                             </div>
                         </div>
                     </div>
                     {/* right => icon */}
-                    <div className={`flex-center size-12 ${Props.title === 'درآمد' ? 'bg-green-100 text-green-500' : Props.title === 'مشتریان' ? 'bg-blue-100 text-blue-500' : 'bg-amber-100 text-amber-500'} rounded-lg *:size-6`}>
+                    <div style={{ color: Props.color,backgroundColor:`${Props.color}33`}} className={`flex-center size-12 rounded-lg *:size-6`}>
                         {Props.icon}
                     </div>
                 </div>
@@ -64,36 +54,36 @@ export default function StatisticsCardsItem(Props) {
                 <ChartContainer className={"h-30 w-full"} config={chartConfig}>
                     <AreaChart
                         accessibilityLayer
-                        data={chartData}
+                        data={Props.chartData}
                     >
                         <XAxis
                             dataKey="month"
                             tickLine={false}
                             axisLine={false}
                             tickMargin={8}
-                            // tickFormatter={(value) => value.slice(0, 3)}
+                        // tickFormatter={(value) => value.slice(0, 3)}
                         />
                         <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
                         <defs>
-                            <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+                            <linearGradient id={Props.id} x1="0" y1="0" x2="0" y2="1">
                                 <stop
                                     offset="5%"
-                                    stopColor="var(--color-desktop)"
+                                    stopColor="var(--color-data)"
                                     stopOpacity={0.8}
                                 />
                                 <stop
                                     offset="95%"
-                                    stopColor="var(--color-desktop)"
+                                    stopColor="var(--color-data)"
                                     stopOpacity={0.1}
                                 />
                             </linearGradient>
                         </defs>
                         <Area
-                            dataKey="desktop"
+                            dataKey="data"
                             type="natural"
-                            fill="url(#fillDesktop)"
+                            fill={`url(#${Props.id})`}
                             fillOpacity={0.4}
-                            stroke="var(--color-desktop)"
+                            stroke="var(--color-data)"
                             stackId="a"
                         />
                     </AreaChart>
